9a259510ae031d602620d80ed91c03f84681e45f
Sangji <133099077+SangjiHan@users.noreply.github.com>
2024-06-09 21:51:47 +0900

    스킬 추가

--- script/home.js
+++ script/home.js
@@ -1,25 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const text = document.querySelector("#texthidden");
-    const content = text.textContent; // text 요소의 텍스트를 가져옵니다.
-    const textshow = document.querySelector("#textshow")
-    text.style.display = "inline-block";
-    text.style.opacity = "0";
-    
-    console.log(content); // 텍스트의 텍스트를 콘솔에 출력합니다.
-
+    const content = "Hi, I'm Sangji. \n front-end developer.";
+    const text = document.querySelector("#text");
     let i = 0;
-    const intervalId = setInterval(function() {
-        if (i < content.length) {
-            // 줄바꿈 처리
-            if (content[i] === "\n") {
-                textshow.innerHTML += "<br>";
-            } else {
-                textshow.innerHTML += content[i];
-            }
-            textshow.style.opacity = "1"; 
-            i++;
-        } else {
-            clearInterval(intervalId); 
+
+    function typing(){
+        let txt = content[i++];
+        text.innerHTML += txt === "\n" ? "<br/>" : txt;
+        if (i >= content.length) {
+            clearInterval(intervalId);
         }
-    }, 100); 
+    }
+
+    const intervalId = setInterval(typing, 180);
 });
+
+
+setTimeout(() => {
+    const element = document.querySelector('#blink');
+    element.style.opacity = '0'; 
+}, 8000);
