4cd8604caea2cf4db8fa17e6f33bcad963e80ab0
Sangji <133099077+SangjiHan@users.noreply.github.com>
2024-06-22 14:40:15 +0900

    애니메이션 수정

--- script/goToScroll.js
+++ script/goToScroll.js
@@ -1,4 +1,40 @@
 function goToScroll(name) {
-    var location = document.querySelector("#" + name).offsetTop;
-    window.scrollTo({top:location, behavior:'smooth'});
+    var element = document.querySelector("#" + name);
+    if (element) {
+      var location = element.offsetTop;
+      window.scrollTo({ top: location, behavior: 'smooth' });
+    }
+  }
+
+
+let windowHeight = window.innerHeight
+
+
+const aboutMe = ()=>{
+    const abutMeArea = document.querySelector('#aboutmearea')
+
+
+    if(abutMeArea.getBoundingClientRect().top < windowHeight-500){
+        setTimeout(()=>{
+            abutMeArea.style.animation = 'fadeInUp 2s'
+            abutMeArea.style.opacity = 1
+        },200) 
+        window.removeEventListener('scroll',aboutMe)
+    }
 }
+window.addEventListener('scroll',aboutMe)
+
+const skills = ()=>{
+    const skillsArea = document.querySelector('#skillsarea')
+    console.log(skillsArea);
+
+    if(skillsArea.getBoundingClientRect().top < windowHeight-600){
+        setTimeout(()=>{
+            skillsArea.style.animation = 'fadeInUp 2s'
+            skillsArea.style.opacity = 1
+        },200) 
+        window.removeEventListener('scroll',skills)
+    }
+}
+window.addEventListener('scroll',skills)
+
